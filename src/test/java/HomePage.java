177c19e68c45e706d7bb75b31eb489c9e53e6181
Viacheslav Ivanyshyn <sslava543@gmail.com>
2019-02-28 17:09:26 +0100

    Hometask to lesson #7. Added login method to return different PageObjects (HomePage or LoginSubmitPage)

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -1,19 +1,18 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
     private WebDriver driver;
 
+    @FindBy(xpath = "//li[@id='profile-nav-item']")
     private WebElement profileMenuItem;
 
     public HomePage (WebDriver driver){
         this.driver=driver;
-        initElements();
-    }
-
-    private void initElements() {
-        profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+        PageFactory.initElements(driver, this);
     }
 
     public boolean isPageLoaded() {
